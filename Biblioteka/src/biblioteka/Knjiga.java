7983631421eb42577dd68b811cdc2fee8da25f31
kalubre23 <luka.zola@hotmail.com>
2024-03-25 16:23:00 +0100

    Dodati atributi i get i set metode za te atribute

--- Biblioteka/src/biblioteka/Knjiga.java
+++ Biblioteka/src/biblioteka/Knjiga.java
@@ -2,7 +2,29 @@ package biblioteka;
 
 public class Knjiga {
 
-	private static String naziv;
-	//sad sam još nešta ispisao a dodao sam fajl već u stage
-	//
+	private String naziv;
+	private String autori;
+	private int izdanje;
+	
+	
+	public String getNaziv() {
+		return naziv;
+	}
+	public void setNaziv(String naziv) {
+		this.naziv = naziv;
+	}
+	public String getAutori() {
+		return autori;
+	}
+	public void setAutori(String autori) {
+		this.autori = autori;
+	}
+	public int getIzdanje() {
+		return izdanje;
+	}
+	public void setIzdanje(int izdanje) {
+		this.izdanje = izdanje;
+	}
+	
+
 }
