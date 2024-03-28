08dc18845893bef8a8742c13fd31b36bf30105a3
kalubre23 <luka.zola@hotmail.com>
2024-03-28 13:46:23 +0100

    Napravljena klasa Biblioteka koja implementira interfejs BibiliotekaInterfejs

---
+++ Biblioteka/src/biblioteka/Biblioteka.java
@@ -0,0 +1,41 @@
+package biblioteka;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import biblioteka.interfejs.BibliotekaInterfejs;
+
+public class Biblioteka implements BibliotekaInterfejs {
+
+	List<Knjiga> knjige = new ArrayList<>();
+	
+	
+	@Override
+	public void dodajKnjigu(Knjiga knjiga) {
+		knjige.add(knjiga);
+	}
+
+	@Override
+	public void obrisiKnjigu(Knjiga knjiga) {
+		knjige.remove(knjiga);
+	}
+
+	@Override
+	public List<Knjiga> vratiSveKnjige() {
+		return knjige;
+	}
+
+	@Override
+	public List<Knjiga> pronadjiKnjigu(Autor autor, int isbn, String naslov, String izdavac) {
+		List<Knjiga> result = new ArrayList<>();
+		
+		for(Knjiga k: knjige) {
+			if(k.getNaziv().toLowerCase().contains(naslov.toLowerCase().trim())) {
+				result.add(k);
+			}
+		}
+		
+		return result;
+	}
+
+}
