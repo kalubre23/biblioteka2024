e75f4ee705cfea01fbc3133158180539237624c9
kalubre23 <luka.zola@hotmail.com>
2024-03-28 14:01:14 +0100

    DOdata logicka kontrola na klasi Biblioteka

--- Biblioteka/src/biblioteka/Biblioteka.java
+++ Biblioteka/src/biblioteka/Biblioteka.java
@@ -12,21 +12,30 @@ public class Biblioteka implements BibliotekaInterfejs {
 	
 	@Override
 	public void dodajKnjigu(Knjiga knjiga) {
+		if(knjiga==null)
+			throw new NullPointerException("Knjiga je null!");
+		if(knjige.contains(knjiga))
+			throw new IllegalArgumentException("Knjiga vec postoji u biblioteci!");
 		knjige.add(knjiga);
 	}
 
 	@Override
 	public void obrisiKnjigu(Knjiga knjiga) {
+		//Nije potrebna logicka kontrola ovde, pitanje je dal ocemo tiho da prodje ovo
 		knjige.remove(knjiga);
 	}
 
 	@Override
 	public List<Knjiga> vratiSveKnjige() {
+		//ne treba logicka kontrola
 		return knjige;
 	}
 
 	@Override
 	public List<Knjiga> pronadjiKnjigu(Autor autor, int isbn, String naslov, String izdavac) {
+		if(autor==null && isbn<=0 && naslov==null && izdavac==null)
+			throw new IllegalArgumentException("Morate uneti bar neki kriterijum pretrage!");
+		
 		List<Knjiga> result = new ArrayList<>();
 		
 		for(Knjiga k: knjige) {
